2453fffadeb9afa51737316e54829860b9ecbee2
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-22 21:21:48 -0700

    iOS

--- Lesson-3_Synchronisation_Mutex.playground/Contents.swift
+++ Lesson-3_Synchronisation_Mutex.playground/Contents.swift
@@ -5,4 +5,36 @@
 // If not syncrinized - broken data
 
 
+/*
+ 
+ Mutex — ensures that only one thread is active in a given region of code at a time. You can think of it as a semaphore with a maximum count of 1.
+ 
+ Mutex is blocking and releasing objects
+ 
+ when the object is Mutex protected - the thread pinging it is in WAIT mode until Mutex is released
+ */
+
+import UIKit
+
+
+class SaveThread{
+    private var mutex = pthread_mutex_t()
+    
+    init(){
+        pthread_mutex_init(&mutex, nil)
+    }
+    
+    func someMethod(completion: ()->Void){
+        
+        //locking our object
+        pthread_mutex_lock(&mutex)
+        
+        // some data blah-blah
+        
+        //unlocking object
+        pthread_mutex_unlock(&mutex)
+    }
+}
+
+
 
