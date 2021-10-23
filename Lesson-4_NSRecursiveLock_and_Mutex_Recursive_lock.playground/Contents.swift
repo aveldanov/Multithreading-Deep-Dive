17242fa6ef18d147af8b27d0608f7f1bdc824516
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-22 22:08:45 -0700

    iOS

--- Lesson-4_NSRecursiveLock_and_Mutex_Recursive_lock.playground/Contents.swift
+++ Lesson-4_NSRecursiveLock_and_Mutex_Recursive_lock.playground/Contents.swift
@@ -1,6 +1,8 @@
 
 
 // LESSON 4 - NSRecursiveLock & Mutex Recursive lock
+
+
 /*
  
  Concurrency Problems:
@@ -17,3 +19,48 @@
  
  
  */
+
+/*
+ Do not use NSLock for recursive calls. If you use it twice(because of recursion) it will lock your method forever. Use NSRecursiveLock instead!!!
+ 
+ 
+ */
+
+
+import UIKit
+
+
+//let recursiveLock = NSRecursiveLock()
+
+
+class RecursiveMutexTest{
+    private var mutex = pthread_mutex_t()
+    private var attribute = pthread_mutexattr_t()
+    
+    init(){
+        pthread_mutexattr_init(&attribute)
+        pthread_mutexattr_settype(&attribute, PTHREAD_MUTEX_RECURSIVE)
+        pthread_mutex_init(&mutex, &attribute)
+    }
+    
+    func firstTask(){
+        pthread_mutex_lock(&mutex)
+        secondTask()
+        defer{
+            pthread_mutex_unlock(&mutex)
+        }
+    }
+    
+    private func secondTask(){
+        pthread_mutex_lock(&mutex)
+        print("FINISH")
+        // to unlock even if the method flow is broken
+        defer{
+            pthread_mutex_unlock(&mutex)
+        }
+    }
+}
+
+
+let recursive = RecursiveMutexTest()
+recursive.firstTask()
