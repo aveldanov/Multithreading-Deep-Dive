61f77feb2a3d15729bd342cb7fd48c04c48b1812
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-23 18:56:15 -0700

    iOS

--- Lesson-8_GCD_Pracrice_Sync-Async.playground/Contents.swift
+++ Lesson-8_GCD_Pracrice_Sync-Async.playground/Contents.swift
@@ -1,3 +1,39 @@
-import UIKit
 
-var greeting = "Hello, playground"
+// LESSON 7 - GCD Practice - Sync/Async
+
+import UIKit
+import PlaygroundSupport //helps to wait for threads
+
+
+
+class MyViewController: UIViewController{
+    
+    var button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "vc1"
+        view.backgroundColor = .red
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        initButton()
+    }
+    
+    
+    func initButton(){
+        
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        
+    }
+
+}
+
+
+let vc = MyViewController()
+let navBar = UINavigationController(rootViewController: vc)
+
+PlaygroundPage.current.liveView = navBar
