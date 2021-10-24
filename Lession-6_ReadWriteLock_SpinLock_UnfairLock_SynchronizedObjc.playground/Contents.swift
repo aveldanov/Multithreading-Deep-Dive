ebd547fc0a00d6c9c2c2f6d43a8c4a687b313ddf
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-23 17:56:03 -0700

    iOS

---
+++ Lession-6_ReadWriteLock_SpinLock_UnfairLock_SynchronizedObjc.playground/Contents.swift
@@ -0,0 +1,84 @@
+
+
+// LESSON 6 - ReadWriteLock, SpinLock, UnfairLock, Synchronized in Objc
+
+
+/*
+ more locks besides MUTEX
+ 
+ ReadWriteLock - locks write/read or both. (not OBJ-C environment)
+ */
+
+import Foundation
+import Darwin
+
+class ReadWriteLock{
+    
+    private var lock = pthread_rwlock_t()
+    private var attribute = pthread_rwlockattr_t()
+    
+    private var globalProperty: Int = 0
+    
+    
+    init(){
+        pthread_rwlock_init(&lock, &attribute)
+    }
+    
+    
+    public var workProperty: Int{
+        // rebuiling GETTER and SETTER
+        get{
+            pthread_rwlock_rdlock(&lock)
+            //actions...
+            let temp = globalProperty
+            pthread_rwlock_unlock(&lock)
+            return temp //copy
+        }
+        
+        set{
+            pthread_rwlock_wrlock(&lock)
+            //actions...
+            globalProperty = newValue // newValue is Setter operator
+            pthread_rwlock_unlock(&lock)
+        }
+    }
+    
+}
+
+
+/*
+ SpinLock - faster than mutex....deprecated!!!
+ */
+
+/*
+class SpinLock{
+    private var lock = OS_SPINLOCK_INIT
+    
+    func someMethod(){
+        
+        OSSpinLockLock(&lock)
+        //Action...
+        OSSpinLockUnlock(&lock)
+    }
+}
+*/
+
+
+
+/*
+ UnfairLock replaced SpinLock starting iOS10.0
+ no FOUNDATION
+ */
+
+
+class UnfairLock{
+    private var lock = os_unfair_lock_s()
+    
+    var array = [Int]()
+    
+    func someMethod(){
+        os_unfair_lock_lock(&lock)
+        array.append(1)
+        os_unfair_lock_unlock(&lock)
+    }
+}
