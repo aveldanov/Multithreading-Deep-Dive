41b7685f448cc5f5724623bb4575310bb8e4f560
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-28 14:28:14 -0700

    iOS

--- Lesson-12_GCD_Dispatch_Group.playground/Contents.swift
+++ Lesson-12_GCD_Dispatch_Group.playground/Contents.swift
@@ -10,20 +10,26 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 class DispatchGroupTest1{
     
     private let queueSerial = DispatchQueue(label: "DispatchGroupQueue1")
-    
     private let groupRed = DispatchGroup()
     
-    
     func loadInfo(){
+        queueSerial.async(group: groupRed){
+            sleep(1)
+            print("1")
+            
+        }
         
-        
-        
-        
-        
-        
-        
+        queueSerial.async(group: groupRed){
+            sleep(1)
+            print("2")
+            
+        }
     }
-    
-    
 }
 
+
+
+let dispatchGroupTest1 = DispatchGroupTest1()
+
+
+dispatchGroupTest1.loadInfo()
