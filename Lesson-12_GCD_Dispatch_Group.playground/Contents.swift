9db057c6ebf3067a20d48f95ced552322146690e
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-28 19:38:38 -0700

    iOS

--- Lesson-12_GCD_Dispatch_Group.playground/Contents.swift
+++ Lesson-12_GCD_Dispatch_Group.playground/Contents.swift
@@ -55,7 +55,50 @@ class DispatchGroupTest2{
             print("2")
             self.groupBlack.leave()
         }
+        
+        groupBlack.wait()
+        print("All finished")
+        
+        groupBlack.notify(queue: .main) {
+            print("groupBlack finished all")
+
+        }
     }
 }
 
 let dispatchGroupTest2 = DispatchGroupTest2()
+dispatchGroupTest2.loadInfo()
+
+
+
+
+class EightImage: UIView{
+    
+    public var images = [UIImageView]()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        images.append(UIImageView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)))
+        images.append(UIImageView(frame: CGRect(x: 0, y: 100, width: 100, height: 100)))
+        images.append(UIImageView(frame: CGRect(x: 100, y: 0, width: 100, height: 100)))
+        images.append(UIImageView(frame: CGRect(x: 100, y: 100, width: 100, height: 100)))
+        
+        
+        images.append(UIImageView(frame: CGRect(x: 0, y: 300, width: 100, height: 100)))
+        images.append(UIImageView(frame: CGRect(x: 100, y: 300, width: 100, height: 100)))
+        images.append(UIImageView(frame: CGRect(x: 0, y: 400, width: 100, height: 100)))
+        images.append(UIImageView(frame: CGRect(x: 100, y: 400, width: 100, height: 100)))
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    
+    
+}
