7fd9b6e7c950fc5e372c26eb4aedb03840f780d0
Anton Veldanov <anton.veldanov@gmail.com>
2021-10-28 20:29:47 -0700

    iOS

--- Lesson-13_GCD_DispatchBarrier.playground/Contents.swift
+++ Lesson-13_GCD_DispatchBarrier.playground/Contents.swift
@@ -5,3 +5,27 @@ import UIKit
 import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
+
+var array = [Int]()
+
+// Serial queue
+//for i in 0...9{
+//    array.append(i)
+//}
+//print(array, array.count)
+
+
+// Same thing but in concurrent
+
+//DispatchQueue.concurrentPerform(iterations: 10) { index in
+//    array.append(index)
+//}
+//// Shows race condition - array is messy and not full: [3, 5, 6, 7, 8, 9] 6
+
+//print(array, array.count)
+
+
+class SafeArray(){
+    
+    
+}
